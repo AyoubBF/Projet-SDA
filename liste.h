8f4217f60de4630c73c42e4875b1129cffec659f
AyoubBF <72936702+AyoubBF@users.noreply.github.com>
2021-01-08 13:41:11 +0100

    Update liste.h

--- liste.h
+++ liste.h
@@ -1,9 +1,16 @@
 #pragma once
 
-const unsigned int MAX_LETTRES = 15;
+const unsigned int MAX_LETTRES = 30;
 
+/**
+ * @brief tableau de caractères formant un mot
+*/
 typedef char Mot[MAX_LETTRES + 1];
 
+/**
+ * @brief structure d'une liste de mot avec ses champs permettant de stocké
+ * le nombre de mots, le nombre de points et les paramètres de dynamicité
+*/
 struct Liste {
 	Mot* tab_mots;
 	unsigned int capa;
